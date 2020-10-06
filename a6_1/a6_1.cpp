fa99137712d2e1711b8a35bca078109541fddd78
nyccowgirl <nyc_cowgirl@hotmail.com>
2020-10-06 01:19:20 -0500

    fixing for outdated git and local

--- a6_1/a6_1.cpp
+++ a6_1/a6_1.cpp
@@ -7,8 +7,8 @@
  
  Assignment 6.1
  
- Program illustrates the use of different ways that string data can be handled in C++
- without using the string library.
+ Program illustrates the use of different ways that string data (e.g., C-strings) can be
+ handled in C++ without using the string library.
  
  Created by nyccowgirl on 10/6/20.
  Copyright © 2020 nyccowgirl. All rights reserved.
@@ -29,54 +29,57 @@ int numLetters(const char* inString);
 
 
 int main(int argc, const char * argv[]) {
-//    char str1[] = "Hello World!";            // To define test character arrays
-    char str2[] = "C++ is fun!";
+    char str1[] = "Hello World!";            // To define test character arrays
+//    char str2[] = "C++ is fun!";
 //    char str3[] = "racecar";
 //    char str4[] = "aBBA";
-    char target = 'a', replacement = 'z';
-    int userOpt1 = 0, userOpt2 = 0;
-    long userLength;
-    char userStr[100];
+    char target = 'a', replacement = 'z';   // To set default target and replacement char
+    int userOpt1 = 0, userOpt2 = 0;         // To set default user options to input
+    char userStr[100];                      // To define array to hold user input C-string
     char *inString;                         // To point to a character array
     
-    do {
-        cout << "Do you want to enter your own string?: (0 = No; 1 = Yes) ";
-        cin >> userOpt1;
-        cin.ignore();
-        
-        if (!(userOpt1 == 0 || userOpt1 == 1)) {
-            cout << "Invalid input: Please enter 0 for No and 1 for Yes." << endl;
-        }
-    } while (!(userOpt1 == 0 || userOpt1 == 1));
+//    cout << strlen(str1) << endl;
+//    cout << sizeof(str1) << endl;
+    // Asks if user wants to provide customized string
+//    do {
+//        cout << "Do you want to enter your own string?: (0 = No; 1 = Yes) ";
+//        cin >> userOpt1;
+//        cin.ignore();
+//
+//        if (!(userOpt1 == 0 || userOpt1 == 1)) {
+//            cout << "Invalid input: Please enter 0 for No and 1 for Yes." << endl;
+//        }
+//    } while (!(userOpt1 == 0 || userOpt1 == 1));
     
+    // If yes, gets user's input limited to 99 characters
     if (userOpt1 == 1) {
-        cout << "Enter your test string: ";
+        cout << "Enter your test string (limited to less than 100 characters): ";
         cin.get(userStr, 100, '\n');
         cin.ignore();
-        userLength = strlen(userStr);
 //        cout << userStr << endl;
 //        cout << strlen(userStr) << endl;
         inString = userStr;
     } else {
-//        inString = str1;                    // Modify array name from options above to test
-        inString = str2;
+        inString = str1;                    // Modify array from options above to test
+//        inString = str2;
 //        inString = str3;
 //        inString = str4;
     }
     
 //    cout << "Test string: " << inString << endl;
-    
-    do {
-        cout << "Do you want to enter your own target and replacement characters?:";
-        cout << "(0 = No; 1 = Yes) ";
-        cin >> userOpt2;
-        cin.ignore();
-        
-        if (!(userOpt2 == 0 || userOpt2 == 1)) {
-            cout << "Invalid input: Please enter 0 for No and 1 for Yes." << endl;
-        }
-    } while (!(userOpt2 == 0 || userOpt2 == 1));
-    
+    // Asks if user wants to modify target and replacement characters from default
+//    do {
+//        cout << "Do you want to enter your own target and replacement characters?:";
+//        cout << "(0 = No; 1 = Yes) ";
+//        cin >> userOpt2;
+//        cin.ignore();
+//
+//        if (!(userOpt2 == 0 || userOpt2 == 1)) {
+//            cout << "Invalid input: Please enter 0 for No and 1 for Yes." << endl;
+//        }
+//    } while (!(userOpt2 == 0 || userOpt2 == 1));
+//
+    // If yes, gets user's inputs for both
     if (userOpt2 == 1) {
         cout << "Enter your target character: ";
         cin >> target;
@@ -87,30 +90,41 @@ int main(int argc, const char * argv[]) {
     }
     
 //    cout << "Target - Replacement: " << target << " - " << replacement << endl;
+    
+    for (int x = 0; x < strlen(inString); x++) {
+        cout << x << ": " << inString[x] << endl;
+    }
+    
+    // Tests function lastIndexOf
     cout << "\nThe last index with character '" << target << "': ";
     cout << lastIndexOf(inString, target) << endl;
     
+    // Tests function reverse (and returns string back to original for other test functions)
     reverse(inString);
     cout << "\nThe reverse of the string: " << inString << endl;
-    reverse(inString);                      // To restore original string
+    reverse(inString);                      // To restore to original string
     
+    // Tests function replace (and returns string back to original for other test functions)
     cout << "\nNumber of times the character '" << target;
     cout << "' was replaced by '" << replacement << "': ";
     cout << replace(inString, target, replacement) << endl;
     cout << "The modified string: " << inString << endl;
-    replace(inString, replacement, target); // To restore original string
-    
+    replace(inString, replacement, target); // To restore to original string
+
+    // Tests function isPalindrome
     cout << "\nIs the string a palindrome? ";
     if (isPalindrome(inString)) {
         cout << "Yes" << endl;
     } else {
         cout << "No" << endl;
     }
-
+    
+    // Tests function toupper
     toupper(inString);
     cout << "\nThe string in all UPPER CASE letters: ";
     cout << inString << endl;
     
+    // Tests function numLetters
     cout <<"\nThe number of letters in the string is: " << numLetters(inString) << endl;
     
     return 0;
@@ -129,11 +143,21 @@ int main(int argc, const char * argv[]) {
 
 int lastIndexOf(const char* inString, char target) {
     int index = -1;
+    int x = 0;
     
-    for (int x = 0; x < strlen(inString); x++) {
-        if (*(inString + x) == target) {
+//    cout << strlen(inString) << endl;
+//    for (int x = 0; x < strlen(inString); x++) {
+//        if (*(inString + x) == target) {
+//            index = x;
+//        }
+//    }
+    
+    // using while loop
+    while (inString[x] != '\0') {
+        if (inString[x] == target) {
             index = x;
         }
+        x++;
     }
     return index;
 }
@@ -170,12 +194,22 @@ void reverse(char* inString) {
 
 int replace(char* inString, char target, char replacementChar) {
     int count = 0;
+    int x = 0;
     
-    for (int x = 0; x < strlen(inString); x++) {
-        if (*(inString + x) == target) {
-            *(inString + x) = replacementChar;
+//    for (int x = 0; x < strlen(inString); x++) {
+//        if (*(inString + x) == target) {
+//            *(inString + x) = replacementChar;
+//            count++;
+//        }
+//    }
+    
+    // using while loop
+    while (inString[x] != '\0') {
+        if (inString[x] == target) {
+            inString[x] = replacementChar;
             count++;
         }
+        x++;
     }
     return count;
 }
@@ -195,7 +229,7 @@ bool isPalindrome(const char* inString) {
     long backwards = strlen(inString);
     
     for (int x = 0; x < (backwards / 2); x++) {
-        if (toupper(*(inString + x)) != toupper(*(inString + backwards - x -1))) {
+        if (toupper(*(inString + x)) != toupper(*(inString + backwards - x - 1))) {
             return false;
         }
     }
@@ -213,8 +247,16 @@ bool isPalindrome(const char* inString) {
  */
 
 void toupper(char* inString) {
-    for (int x = 0; x < strlen(inString); x++) {
+    int x = 0;
+    
+//    for (int x = 0; x < strlen(inString); x++) {
+//        *(inString + x) = toupper(*(inString + x));
+//    }
+    
+    //using while loop
+    while (inString[x] != '\0') {
         *(inString + x) = toupper(*(inString + x));
+        x++;
     }
 }
 
@@ -230,13 +272,62 @@ void toupper(char* inString) {
 
 int numLetters(const char* inString) {
     int count = 0;
+    int x = 0;
     
-    for (int x = 0; x < strlen(inString); x++) {
-        if (isalpha(*(inString + x))) {
+//    for (int x = 0; x < strlen(inString); x++) {
+//        if (isalpha(*(inString + x))) {
+//            count++;
+//        }
+//    }
+    
+    // using while loop
+    while (inString[x] != '\0') {
+        if (isalpha(inString[x])) {
             count++;
         }
+        x++;
     }
     return count;
 }
 
 
+/*
+ 
+ Do you want to enter your own string?: (0 = No; 1 = Yes) 1
+ Enter your test string (limited to less than 100 characters: People say nothing is impossible but I do nothing every day.
+ Do you want to enter your own target and replacement characters?:(0 = No; 1 = Yes) 0
+
+ The last index with character 'a': 57
+
+ The reverse of the string: .yad yreve gnihton od I tub elbissopmi si gnihton yas elpoeP
+
+ Number of times the character 'a' was replaced by 'z': 2
+ The modified string: People szy nothing is impossible but I do nothing every dzy.
+
+ Is the string a palindrome? No
+
+ The string in all UPPER CASE letters: PEOPLE SAY NOTHING IS IMPOSSIBLE BUT I DO NOTHING EVERY DAY.
+
+ The number of letters in the string is: 49
+ Program ended with exit code: 0
+ 
+ *****
+ 
+ Do you want to enter your own string?: (0 = No; 1 = Yes) 0
+ Do you want to enter your own target and replacement characters?:(0 = No; 1 = Yes) 0
+
+ The last index with character 'a': 0
+
+ The reverse of the string: ABBa
+
+ Number of times the character 'a' was replaced by 'z': 1
+ The modified string: zBBA
+
+ Is the string a palindrome? Yes
+
+ The string in all UPPER CASE letters: ABBA
+
+ The number of letters in the string is: 4
+ Program ended with exit code: 0
+ 
+ */
